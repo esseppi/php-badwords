46def8218297c8f504856dd26011a85966c55572
Your Name <you@example.com>
2022-04-11 16:42:26 +0200

    fine

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>PHP Badwords</title>
 </head>
+
 <body>
+   <form action="" method="GET">
+      <input type="text" name="search">
+      <button>submit</button>
 
-<?php 
-$text = 'bella';
-echo $text
+   </form>
 
-?>
+   <?php
+   $text = 'Inserisci una o più lettere contenute in questa frase per nasconderle';
+
+   if (isset($_GET['search'])) {
+      $findWord = $_GET['search'];
+      $badText = str_replace($findWord, '***', $text);
+      $len = strlen($badText);
+      echo "$badText <br> $len lettere";
+   } else {
+      echo $text;
+   }
+
+   ?>
 
 </body>
+
 </html>
